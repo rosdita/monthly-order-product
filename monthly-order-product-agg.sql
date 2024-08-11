e5d230f5d76feb351e7ed9af6206ceb315b7203f
rosdita <rosditanazizah@gmail.com>
2024-08-12 00:08:23 +0700

    Update and rename monthly-order-product-agg to monthly-order-product-agg.sql
    Remove the explanation and make a new file for it.

---
+++ monthly-order-product-agg.sql
@@ -0,0 +1,20 @@
+-- Monthly sales order based on product category
+CREATE TEMPORARY TABLE report_monthly_orders_product_agg AS
+SELECT b.category AS product_category,
+  DATE(DATE_TRUNC (a.created_at, MONTH)) AS month,
+  SUM(a.sale_price) AS sales
+FROM `bigquery-public-data.thelook_ecommerce.order_items` AS a
+  LEFT JOIN `bigquery-public-data.thelook_ecommerce.products` AS b
+  ON a.product_id = b.id
+WHERE a.status = 'Complete'
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+-- Monthly Top-Selling Products
+SELECT *
+FROM report_monthly_orders_product_agg AS a
+WHERE sales = 
+  (SELECT MAX(sales)
+  FROM report_monthly_orders_product_agg AS b
+  WHERE a.month = b.month)
+ORDER BY month;

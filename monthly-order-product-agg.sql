c771435fa956b8af96e480f9adae76f47ee63b85
rosdita <rosditanazizah@gmail.com>
2024-08-12 08:50:27 +0700

    Update monthly-order-product-agg.sql
    Add query for Monthly Top-Selling Products by Sales Quantity.

--- monthly-order-product-agg.sql
+++ monthly-order-product-agg.sql
@@ -2,6 +2,7 @@
 CREATE TEMPORARY TABLE report_monthly_orders_product_agg AS
 SELECT b.category AS product_category,
   DATE(DATE_TRUNC (a.created_at, MONTH)) AS month,
+  COUNT(a.product_id) AS items_sold,
   SUM(a.sale_price) AS sales
 FROM `bigquery-public-data.thelook_ecommerce.order_items` AS a
   LEFT JOIN `bigquery-public-data.thelook_ecommerce.products` AS b
@@ -15,6 +16,15 @@ SELECT *
 FROM report_monthly_orders_product_agg AS a
 WHERE sales = 
   (SELECT MAX(sales)
+  FROM report_monthly_orders_product_agg AS b
+  WHERE a.month = b.month)
+ORDER BY month;
+
+-- Monthly Top-Selling Products by Sales Quantity
+SELECT *
+FROM report_monthly_orders_product_agg AS a
+WHERE items_sold = 
+  (SELECT MAX(items_sold)
   FROM report_monthly_orders_product_agg AS b
   WHERE a.month = b.month)
 ORDER BY month;
